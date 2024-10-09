bc316db2ea3ea1f0041e960017460f0ba4e2b458
m-saqlain024 <m.saqlain0241@gmail.com>
2024-10-09 14:46:50 +0500

    create routes

---
+++ backend/controller/userContainer.js
@@ -0,0 +1,3 @@
+const authUser = (req ,res)=>{
+    res.status(300).json({message : "User is a auth"})
+}
--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,11 @@
 import express from "express";
-import dotenv from "dotenv"
-dotenv.config
-
+import dotenv from "dotenv";
+dotenv.config();
+import userRoutes from "./routes/userRoutes.js";
 const port = process.env.PORT || 5000;
 
-const app = express()
+const app = express();
+app.use("api/user", userRoutes);
+app.get("/", (req, res) => res.send("hello world"));
 
-app.get('/', (req ,res)=> res.send("hello world"))
-
-
-app.listen(port, ()=> console.log(`Server started at ${port}`))
+app.listen(port, () => console.log(`Server started at ${port}`));
