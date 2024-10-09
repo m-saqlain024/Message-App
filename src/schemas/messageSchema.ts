4b5a87825a2a4fc6655851520c7017a34601cad8
m-saqlain024 <m.saqlain0241@gmail.com>
2024-10-09 13:46:21 +0500

    create a backend server and api

--- src/schemas/messageSchema.ts
+++
@@ -1,8 +0,0 @@
-import { z } from "zod";
-
-export const messageSchema = z.object({
-  content: z
-    .string()
-    .min(10, { message: "content must be atleast 10 character" })
-    .max(300, { message: "content must no longer 300 character" }),
-});
